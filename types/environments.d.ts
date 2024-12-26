7bfc35877b3d37aa704f8c7b7bd6922018ce2791
Felipe Seabra <feliperoce@gmail.com>
2024-12-26 11:48:37 -0300

    fix: remove variable to types

--- types/environments.d.ts
+++ types/environments.d.ts
@@ -1,7 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 namespace NodeJS {
   export interface ProcessEnv {
-    NEXT_PUBLIC_EMAILJS_USER_ID: string
     NEXT_PUBLIC_ANALYTICS_ID: string
     NEXT_PUBLIC_API_URL: string
     NEXT_PUBLIC_SUPABASE_STORAGE_LINK: string
