068a6e2f1c074c1bb539afa6e9301f0ddbbca396
Felipe Seabra <feliperoce@gmail.com>
2024-12-26 13:06:37 -0300

    feat: fix not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -17,7 +17,7 @@ export default function Error404() {
           <Link href="/" className="relative z-20 flex justify-center">
             <Button
               type="button"
-              className="bg-criarte-secundary-700 hover:bg-criarte-secundary"
+              className="bg-color-secundary-700 hover:bg-color-secundary"
             >
               VOLTAR
             </Button>
