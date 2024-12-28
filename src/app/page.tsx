a470ca3c285a887785c180416463adbf46fa3720
Felipe Seabra <feliperoce@gmail.com>
2024-12-28 11:35:42 -0300

    fix: fix name to components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import { Suspense } from 'react'
 
 import LoadingHomePage from './loading'
-import HomePageComponent from './_components/home/home-componet'
-import BenefitsComponent from './_components/benefits/benefits-componet'
-import PlansComponent from './_components/plans/plans-componet'
-import FaqComponent from './_components/faq/faq-componet'
+import HomePageComponent from './_components/home/home'
+import BenefitsComponent from './_components/benefits/benefits'
+import PlansComponent from './_components/plans/plans'
+import FaqComponent from './_components/faq/faq'
 
 export default function Home() {
   return (
