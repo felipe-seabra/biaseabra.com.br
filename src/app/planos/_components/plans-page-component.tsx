05ef0e6ff565107b9c817cd4109e6542e1c9b980
Felipe Seabra <feliperoce@gmail.com>
2024-12-28 12:48:04 -0300

    fix: fix name style class

--- src/app/planos/_components/plans-page-component.tsx
+++ src/app/planos/_components/plans-page-component.tsx
@@ -6,7 +6,7 @@ import { plans } from '@/database/plans'
 
 export function PlansPageComponent() {
   return (
-    <div className="bg-criarte-background flex min-h-[84vh] items-center justify-center pt-40">
+    <div className="flex min-h-[84vh] items-center justify-center bg-color-background pt-40">
       <div className="flex max-w-[1800px] flex-wrap items-center justify-center">
         {plans?.map((plan) => (
           <div className="m-3" key={plan.id}>
